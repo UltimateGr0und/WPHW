d49ada570e454288f0dde846402049b3f3531741
UltimateGr0und <mr.sergeim3456@mail.ru>
2023-01-20 21:10:12 +0600

    model added

---
+++ WPHW2/Controllers/AsyncController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WPHW2.Controllers
+{
+    public class AsyncController : Controller
+    {
+        // GET: Async
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Async/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Async/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Async/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Async/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Async/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Async/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Async/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WPHW2/Models/PersonDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace WPHW2.Models
+{
+    public class PersonDBContext : DbContext
+    {
+        public PersonDBContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Person> Employees { get; set; }
+    }
+}
