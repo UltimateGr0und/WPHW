91c28ad4cdbd51f99b14efe6e98143aebdf0e37a
UltimateGr0und <mr.sergeim3456@mail.ru>
2023-02-25 21:30:20 +0600

    ddone

---
+++ WPHW6/Controllers/QuestsController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WPHW6.Models;
+
+namespace WPHW6.Controllers
+{
+    public class QuestsController : Controller
+    {
+        // GET: Quests
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Quests/Details/5
+        public ActionResult Details(int id)
+        {
+            Quest quest = new Quest() { MaxPlayers = 5, MinPlayers = 1, FearLevel = 3, Email = "fear@gmail.com", DifficultyLevel = 3, Company = "Best quests inc", Address = "Pushki st. 34", Name = "AAAAA Strashno", Phone = "88005553535", Rating = 4.9, Time = 3.5, Description = "Long deeeeeeeeescription blablabla bla blablabla balbalbal balblabl", Logo= "https://images.unsplash.com/photo-1500602763988-7d6c9c1263ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80", 
+            Gallery=new List<string> { "https://images.unsplash.com/photo-1633555690973-b736f84f3c1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", "https://images.unsplash.com/photo-1635641398283-ee8f83a6e6f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" }
+            };
+            return View(quest);
+        }
+
+        // GET: Quests/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Quests/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Quests/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Quests/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Quests/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Quests/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WPHW6/Models/Quest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WPHW6.Models
+{
+    public class Quest
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="name is required")]
+        [MaxLength(50,ErrorMessage ="too long name")]
+        [Display(Name="Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "description is required")]
+        [MaxLength(500, ErrorMessage = "too long description")]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Time is required")]
+        [Display(Name = "Time")]
+        public double Time { get; set; }
+        [Required(ErrorMessage = "Min players is required")]
+        [Display(Name = "Minimum players")]
+        public int MinPlayers { get; set; }
+        [Required(ErrorMessage = "Max players is required")]
+        [Display(Name = "Maximum players")]
+        public int MaxPlayers { get; set; }
+        [Required(ErrorMessage = "Address is required")]
+        [MaxLength(50, ErrorMessage = "too long address")]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+        [MaxLength(20, ErrorMessage = "too long phone number")]
+        [Display(Name = "Phone number")]
+        public string Phone { get; set; }
+        [MaxLength(50, ErrorMessage = "too long email")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [MaxLength(50, ErrorMessage = "too long company name")]
+        [Display(Name = "Company")]
+        public string Company { get; set; }
+        [Display(Name = "Rating")]
+        [HiddenInput(DisplayValue = false)]
+        public double Rating { get; set; }
+        [Required(ErrorMessage = "FearLevel is required")]
+        [Display(Name = "Fear level")]
+        public double FearLevel { get; set; }
+        [Required(ErrorMessage = "DifficultyLevel is required")]
+        [Display(Name = "Difficulty level")]
+        public double DifficultyLevel { get; set; }
+        public string Logo { get; set; }
+        public List<string> Gallery { get; set; }
+    }
+}
