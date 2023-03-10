20a51602de615db5ffef0197fae9ef2b9cbd990d
UltimateGr0und <mr.sergeim3456@mail.ru>
2023-03-10 19:10:30 +0600

    1234

---
+++ Exam1/Filters/AdminAuthentificationFilter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc.Filters;
+using System.Web.Mvc;
+using Exam1.Models;
+
+namespace Exam1.Filters
+{
+    public class AdminAuthentificationFilter : FilterAttribute, IAuthenticationFilter
+    {
+        private AccountDBContext db = new AccountDBContext();
+        // GET: Accounts
+        private Account RegistratedAccount(string ip)
+        {
+            try
+            {
+                return db.Accounts.Where(
+                a => a.Sessions.Where(
+                    s => s.Ip == ip).Any()).Single();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            Account account = RegistratedAccount(filterContext.HttpContext.Request.UserHostAddress);
+            if (account == null || account.AccountType != AccountType.Admin)
+            {
+                filterContext.Result = new HttpUnauthorizedResult();
+            }
+        }
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            Account account = RegistratedAccount(filterContext.HttpContext.Request.UserHostAddress);
+            if (account == null || account.AccountType != AccountType.Admin)
+            {
+                filterContext.Result = new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary { { "controller", "Registration" }, { "action", "Index" } });
+            }
+        }
+    }
+}
